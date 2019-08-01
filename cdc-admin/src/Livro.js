281f49d15ce3f53bca88b53ab9c16bd3464c81bd
Daniel Mendes <daniel.mendes@totvs.com.br>
2019-08-01 14:11:44 -0300

    Alteração da versão do react por conta de breakchanges do curso e começo da implementação de rotas

---
+++ cdc-admin/src/Livro.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+export class LivroBox extends Component {
+    render() {
+        return (
+            <p>Livros...</p>
+        );
+    }
+}
--- cdc-admin/src/index.js
+++ cdc-admin/src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Router, Route} from 'react-router';
+import { AutorBox } from './Autor';
+import { LivroBox } from './Livro';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    (<Router>
+      <Route path="/" component={App}/>
+      <Route path="/autor" component={AutorBox}/>
+      <Route path="/livro" component={LivroBox}/>
+    </Router>),
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
